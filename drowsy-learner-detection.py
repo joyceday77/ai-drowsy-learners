6ab892cea95cde5c6562f42c86a1847b3961e346
Joyce <joyceday77@hotmail.com>
2023-10-14 20:13:40 +0800

    Update based on feedback

--- drowsy-learner-detection.py
+++ drowsy-learner-detection.py
@@ -45,10 +45,10 @@ args = vars(ap.parse_args())
 
 # 定义常数
 EAR_THRESH = 0.2  # 眼睛长宽比阀值
-EYE_AR_CONSEC_FRAMES = 20  # 连续满足EAR阀值的帧数阀值
+EYE_AR_CONSEC_FRAMES = 10  # 连续满足EAR阀值的帧数阀值
 EYE_CLOSE_THRESH = 60  # 闭眼总计数阀值,超出后发出告警
-MAR_THRESH = 0.8  # 打哈欠长宽比阀值
-MOUTH_AR_CONSEC_FRAMES = 20  # 连续满足MAR阀值的帧数阀值
+MAR_THRESH = 0.6  # 打哈欠长宽比阀值
+MOUTH_AR_CONSEC_FRAMES = 60  # 连续满足MAR阀值的帧数阀值
 # 初始化闭眼帧计数器和闭眼总数
 eCOUNTER = 0
 eTOTAL = 0
@@ -75,11 +75,11 @@ while True:  # 从视频流循环视频帧
     frame = vs.read()      # 读取视频帧
     frame = imutils.resize(frame, width=720)  # 改变视频框尺寸
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 进行灰度化
-    rects = detector(gray, 0) #获取脸部方框
+    rects = detector(gray, 0) #获取脸部位置
     
     for rect in rects:  # 循环脸部位置信息
-        shape = predictor(gray, rect)  # 获取脸部特征位置的信息
-        shape = face_utils.shape_to_np(shape)  #转换为数组的格式
+        landmarks = predictor(gray, rect)  # 获取脸部特征位置的信息
+        shape = face_utils.shape_to_np(landmarks)  #转换为数组的格式
         
         leftEye = shape[lStart:lEnd]   # 提取左眼坐标
         rightEye = shape[rStart:rEnd]  # 提取右眼坐标
@@ -90,28 +90,16 @@ while True:  # 从视频流循环视频帧
         ear = (leftEAR + rightEAR) / 2.0   # 计算左右眼的长宽比，使用平均值作为最终的长宽比EAR
         
         mar = mouth_aspect_ratio(mouth)  # 计算嘴巴的长宽比MAR
-        
-        leftEyeHull = cv2.convexHull(leftEye)  # 获取左眼凸包位置
-        rightEyeHull = cv2.convexHull(rightEye)  # 获取右眼凸包位置
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)  # 画出左眼轮廓
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)  # 画出右眼轮廓
-        mouthHull = cv2.convexHull(mouth)  # 获取嘴巴凸包位置
-        cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)  # 画出嘴巴轮廓
-        
-        left = rect.left()  
-        top = rect.top()
-        right = rect.right()
-        bottom = rect.bottom()
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)  # 进行画图操作,用矩形框标注人脸
+                
         '''
-            分别计算左眼和右眼的评分求平均作为最终的评分,如果小于阈值,则加1,如果连续EYE_AR_CONSEC_FRAMES次都小于阈值,则表示进行了一次闭眼活动,闭眼数到达阀值后发出告警
+            判断是否打瞌睡,如果EAR小于阈值,则加1,如果连续EYE_AR_CONSEC_FRAMES次都小于阈值,则表示进行了一次闭眼活动,闭眼数到达阈值后发出告警
         '''
         if ear < EAR_THRESH:  # 循环,满足条件的,闭眼帧计数器+1
             eCOUNTER += 1
-            if eCOUNTER >= EYE_AR_CONSEC_FRAMES:  # 如果连续EYE_AR_CONSEC_FRAMES帧都闭眼,则表示进行了一次眨眼活动
+            if eCOUNTER >= EYE_AR_CONSEC_FRAMES:  # 连续数帧都闭眼则判断为一次眨眼活动
                 eTOTAL += 1
                 if eTOTAL >= EYE_CLOSE_THRESH :        
-                    cv2.putText(frame, "Drowsiness!!!", (100, 200),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)  # 在屏幕上显示Drowsiness告警
+                    cv2.putText(frame, "Drowsiness!!!", (80, 200),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)  # 在屏幕上显示Drowsiness提醒
                     if not ALARM_ON:
                         ALARM_ON = True  # 开启告警
                         if args["alarm"] !="":
@@ -125,11 +113,11 @@ while True:  # 从视频流循环视频帧
             eCOUNTER = 0   # 重置闭眼帧计数器  
             
         cv2.putText(frame, "Faces: {}".format(len(rects)), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)   # 在屏幕上显示捕获的人脸数量
-        cv2.putText(frame, "Blinks: {}".format(eTOTAL), (150, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)   # 在屏幕上显示闭眼总数,告警后会清零
-        cv2.putText(frame, "eCOUNTER: {}".format(eCOUNTER), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)    # 在屏幕上显示闭眼帧计数器,告警后会清零
+        cv2.putText(frame, "Blinks: {}".format(eTOTAL), (150, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)   # 在屏幕上显示闭眼总数
+        cv2.putText(frame, "eCOUNTER: {}".format(eCOUNTER), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)    # 在屏幕上显示闭眼帧计数器
         cv2.putText(frame, "EAR: {:.2f}".format(ear), (450, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)   # 在屏幕上实时显示眼睛长宽比   
         '''
-            计算张嘴评分,如果小于阈值,则加1,如果连续MOUTH_AR_CONSEC_FRAMES次都小于阈值,则表示打了一次哈欠
+            判断是否打哈欠,如果MAR小于阈值,则加1,如果连续MOUTH_AR_CONSEC_FRAMES次都小于阈值,则表示打了一次张嘴，张嘴数到达阈值后告警
         '''
         if mar > MAR_THRESH: # 循环,满足条件的,打哈欠帧计数器+1
             mCOUNTER += 1           
@@ -142,16 +130,31 @@ while True:  # 从视频流循环视频帧
             mCOUNTER = 0  # 重置嘴帧计数器
             
         cv2.putText(frame, "Yawning: {}".format(mTOTAL), (150, 60),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2) # 在屏幕上显示打哈欠帧计数器
-        cv2.putText(frame, "mCOUNTER: {}".format(mCOUNTER), (300, 60),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)  # 在屏幕上显示打哈欠总数
+        cv2.putText(frame, "mCOUNTER: {}".format(mCOUNTER), (300, 60),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)  # 在屏幕上显示打哈欠计数器
         cv2.putText(frame, "MAR: {:.2f}".format(mar), (480, 60),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2) # 在屏幕上实时显示嘴巴长宽比 
         
-        for (x, y) in shape:  # 进行画图操作，标记68个特征点标识
-            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1) 
-            
+        # 标注人脸，双眼和嘴巴位置
+        left = rect.left()  
+        top = rect.top()
+        right = rect.right()
+        bottom = rect.bottom()
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)  # 用矩形框标注人脸
+        leftEyeHull = cv2.convexHull(leftEye)  # 获取左眼凸包位置
+        rightEyeHull = cv2.convexHull(rightEye)  # 获取右眼凸包位置
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)  # 画出左眼轮廓
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)  # 画出右眼轮廓
+        mouthHull = cv2.convexHull(mouth)  # 获取嘴巴凸包位置
+        cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)  # 画出嘴巴轮廓
+        
+        for i, point in enumerate(landmarks.parts()):  # 遍历特征点并在图像上绘制
+            x, y = point.x, point.y
+            cv2.circle(frame, (x, y), 3, (0, 0, 255), -1)  # 在特征点位置绘制红色圆圈
+            cv2.putText(frame, str(i + 1), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)  # 在特征点位置标出编号
+
 #    print('眼睛实时长宽比:{:.2f} '.format(ear)+"\t是否眨眼:"+str([False,True][eTOTAL>=60]))
 #    print('嘴巴实时长宽比:{:.2f} '.format(mar)+"\t是否张嘴:"+str([False,True][mTOTAL>=60]))
     
-    cv2.putText(frame, "Press 'q': Quit", (20, 500),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (84, 255, 159), 2)      # 按q退出
+    cv2.putText(frame, "Press 'q' to Quit", (10, 370),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (84, 255, 159), 2)      # 按q退出
     cv2.imshow("Frame", frame)  # 显示视频框，名Frame
     if cv2.waitKey(1) & 0xFF == ord('q'):  # 输入q就退出
         break
